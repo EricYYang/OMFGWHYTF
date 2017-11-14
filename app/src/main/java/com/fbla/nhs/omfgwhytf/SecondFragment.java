ce16fc80f39ceb0c7041d141ff6104c14290b68c
Eric Yang <18yang692@sasd.net>
2017-11-13 18:25:45 -0600

    Initial commit

---
+++ app/src/main/java/com/fbla/nhs/omfgwhytf/SecondFragment.java
@@ -0,0 +1,23 @@
+package com.fbla.nhs.omfgwhytf;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ERIC on 11/10/2017.
+ */
+
+public class SecondFragment extends Fragment {
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.second_layout, container, false);
+        return myView;
+    }
+}
